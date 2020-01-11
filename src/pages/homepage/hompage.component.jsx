823a42dd78a11f3d2adb63089252e000219b0d63
neo8848 <neosudarshan5@gmail.com>
2020-01-11 15:50:25 +1100

    Css files and structure organization

---
+++ src/pages/homepage/hompage.component.jsx
@@ -0,0 +1,15 @@
+import React from 'react'; 
+import App from '../../App';
+import './homepage.style.scss';
+import Directory from '../../components/directory/directory.component';
+
+const HomePage=()=>(
+
+<div className='homepage'>
+    <Directory/>
+</div>
+
+
+);
+
+export default HomePage;
