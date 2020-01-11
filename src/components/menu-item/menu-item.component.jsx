5bcd26647573b01bfadde6a7e1937b680044d51e
neo8848 <neosudarshan5@gmail.com>
2020-01-11 23:34:12 +1100

    menu item styles and home page destructuring

--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -4,12 +4,17 @@ import './menu-item.styles.scss';
 const MenuItem=({title, imageUrl, size})=>(
 
     <div 
+   
+    className={`${size} menu-item`}>
+    
+    <div className='background-image'
     style={{
         backgroundImage:`url(${imageUrl})`
     }} 
-    className={`${size} menu-item`}>
+    
+    />
     <div className='content'>
-<h1 className='title'>{title}</h1>
+<h1 className='title'>{title.toUpperCase()}</h1>
         <span className='subtitles'>shop now</span>
     </div>
 </div>
