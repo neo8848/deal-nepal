0fe8d331d03e69485ba1bececf4f92a748e5c0c6
neo8848 <neosudarshan5@gmail.com>
2020-01-12 00:25:17 +1100

    routes test, security update

--- src/App.js
+++ src/App.js
@@ -1,12 +1,28 @@
 import React, { Component } from 'react';
 import HomePage from './pages/homepage/hompage.component';
 import './App.css';
+import {Switch, Route} from 'react-router-dom';
+
+
+
+
+const HatsPage=()=>(
+  <div>
+  <h1>
+  HATSPAGE
+  </h1>
+  </div>
+);
 
 class App extends Component {
   render() {
     return (
     <div>
-      <HomePage/>
+    <switch>
+    <Route exact path='/' component={HomePage}/>
+    <HomePage/>
+    </switch>
+   
      
     </div>
     );
